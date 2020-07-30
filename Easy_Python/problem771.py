48e7ae881a8a4662ab5804768e0421b75a9b74e8
Heather Ransome <siobahnv@gmail.com>
2020-07-29 21:29:20 -0700

    The count...

---
+++ Easy_Python/problem771.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""
+Problem: 771. Jewels and Stones
+
+You're given strings J representing the types of stones 
+that are jewels, and S representing the stones you have.  
+Each character in S is a type of stone you have.  You want 
+to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters 
+in J and S are letters. Letters are case sensitive, so "a" is 
+considered a different type of stone from "A".
+"""
+
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+
+        count = 0
+        for char in J:
+            count += S.count(char)
+        
+        return count
+
+ohohoh = Solution()
+J = "aA"
+S = "aAAbbbb"
+ohohoh.numJewelsInStones(J, S)
