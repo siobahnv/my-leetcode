9b8d0444dc79282a1ec52ed207a4cd37b10201f9
Heather Ransome <siobahnv@gmail.com>
2020-09-11 12:31:26 -0700

    Setup easy problem in JS.

---
+++ Easy_JavaScript/problem1431.js
@@ -0,0 +1,23 @@
+/**
+ * Problem 1431. Kids With the Greatest Number of Candies
+ * 
+ * Given the array candies and the integer extraCandies, 
+ * where candies[i] represents the number of candies that 
+ * the ith kid has.
+ * 
+ * For each kid check if there is a way to distribute 
+ * extraCandies among the kids such that he or she can 
+ * have the greatest number of candies among them. Notice 
+ * that multiple kids can have the greatest number of candies.
+ * 
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    
+};
+
+candies = [2,3,5,1,3]; 
+extraCandies = 3;
+console.log(kidsWithCandies(candies, extraCandies));
